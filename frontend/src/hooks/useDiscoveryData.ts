ff7e01bf5965ef4f9f2fcdae9e2597dccc266d4e
Furkan Akar <efurkanakar@gmail.com>
2025-09-20 19:16:42 +0300

    Delete frontend directory

--- frontend/src/hooks/useDiscoveryData.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from '@tanstack/react-query';
-import { get } from '../api/client';
-import type { DiscoveryDataset } from '../types';
-
-export const useDiscoveryData = () =>
-  useQuery({
-    queryKey: ['discovery', 'dataset'],
-    queryFn: () => get<DiscoveryDataset>('/vis/discovery', { chart: 'hist' })
-  });
