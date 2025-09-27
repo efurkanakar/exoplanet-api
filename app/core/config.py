437cb91c6fea31a995667ebe17ace8ae74e45b2d
Furkan Akar <efurkanakar@gmail.com>
2025-09-27 22:34:23 +0300

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -12,6 +12,7 @@ class Settings(BaseSettings):
     CORS_ORIGINS: list[str] = [
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://efurkanakar.github.io"
     ]
     CORS_ORIGIN_REGEX: str | None = None
     CORS_ALLOW_CREDENTIALS: bool = False
